c5fdb02661af53e4ecd2cafaaf558a17c607dd4e
cm18rb <ryan.brook8129@gmail.com>
2025-03-19 13:18:18 +0000

    The run file has now been updated to run also on Aire, now just waiting on the licence

--- run/run.py
+++ run/run.py
@@ -39,20 +39,32 @@ if __name__=="__main__":
 
  
     print("Arguments checked")
-    Hostname=socket.gethostname()
-    if(Hostname==("login1.arc4.leeds.ac.uk")or(Hostname==("login2.arc4.leeds.ac.uk"))):
-        HPCFLG=1
-    elif(Hostname==("login1.arc3.leeds.ac.uk")or(Hostname==("login2.arc3.leeds.ac.uk"))):
-        HPCFLG=1 
-    else:
-        HPCFLG=0
+    # Get the hostname
+    hostname = socket.gethostname()
+
+    # Initialize HPCFLG
+    HPCFLG = 0
+
+    # Check for specific substrings in the hostname
+    if "arc4" in hostname:
+        HPCFLG = "arc"
+    elif "arc3" in hostname:
+        HPCFLG = "arc"
+    elif "aire" in hostname:
+        HPCFLG = "aire"
+
+    print(f"HPCFLG: {HPCFLG}")
 
     if(HPCFLG==0):
         EXDIR="../EXEC"
         if not os.path.exists("../EXEC"):
             os.mkdir("../EXEC")  
-    else:
+    elif(HPCFLG=="arc"):
         EXDIR="/nobackup/"+getpass.getuser()
+        if not os.path.exists(EXDIR):
+            os.mkdir(EXDIR)
+    elif(HPCFLG=="aire"):
+        EXDIR="/mnt/scratch/"+getpass.getuser()
         if not os.path.exists(EXDIR):
             os.mkdir(EXDIR)
     
@@ -91,10 +103,9 @@ if __name__=="__main__":
     if(inputs["run"]["Geom_flg"] ==0):
         shutil.copy2("../"+inputs["run"]["Molecule"]+"/bondarr.txt",EXDIR1+"/results")
 
-
     os.chdir(EXDIR1)
     EXDIR1=os.getcwd()
-    if HPCFLG == 1:
+    if HPCFLG == "arc":
         # Makes Job Submission sctipt for initial setup
         if inputs["run"]["Geom_flg"] in ["PubChem", "Initial"]:
             file2="Setup_"+inputs["setup"]["Runfolder"]+".sh"
@@ -177,6 +188,60 @@ if __name__=="__main__":
             f.close()
             command = ['qsub','-N','Plasma_'+inputs["setup"]["Runfolder"]+'_'+str(i+2), '-hold_jid', 'Plasma_'+inputs["setup"]["Runfolder"]+'_'+str(i+1), file1]
             subprocess.call(command)
+    elif HPCFLG == "aire":
+    # AIRE job submission script
+        if inputs["run"]["Geom_flg"] in ["PubChem", "Initial"]:
+            file2 = "Setup_" + inputs["setup"]["Runfolder"] + ".sh"
+            with open(file2, "w") as f:
+                f.write("#!/bin/bash\n")
+                f.write("#SBATCH --job-name=Setup_" + inputs["setup"]["Runfolder"] + "\n")
+                f.write("#SBATCH --time=02:00:00\n")
+                f.write("#SBATCH --mem=1G\n")
+                f.write("#SBATCH --cpus-per-task=" + str(inputs["setup"]["cores"]) + "\n")
+                if inputs["run"]["method"] == "QChem":
+                    f.write("mkdir $TMPDIR/qchemlocal\n")
+                    f.write("tar -xzvf /users/" + getpass.getuser() + "/qchem.tar.gz -C $TMPDIR/qchemlocal\n")
+                    f.write('qchemlocal=$TMPDIR/qchemlocal\n')
+                    f.write('export QCHEM_HOME="$qchemlocal"\n')
+                    f.write('export QC="$qchemlocal"\n')
+                    f.write('export QCAUX="$QC/qcaux"\n')
+                    f.write('export QCPROG="$QC/exe/qcprog.exe"\n')
+                    f.write('export QCPROG_S="$QC/exe/qcprog.exe_s"\n')
+                    f.write('export PATH="$PATH:$QC/exe:$QC/bin"\n')
+                    f.write("export QCSCRATCH=" + EXDIR1 + "/setup/tmp \n")
+                f.write("cd " + EXDIR1 + "/setup \n")
+                f.write("python /users/"+getpass.getuser()+"/PlasmaDissociation-python/code/setup.py")
+            setup_command = ['sbatch', '--parsable', file2]
+            setup_job_id = subprocess.check_output(setup_command).strip().decode('utf-8')
+            print(setup_job_id)
+            exit()
+        file1="Plasma_"+inputs["setup"]["Runfolder"]+"_1.sh"
+        f=open(file1,"w")
+        f.write("#!/bin/bash\n")
+        f.write("#SBATCH --mem=1G")
+        f.write("#SBATCH --time="+str(inputs["setup"]["hours"])+":00:00\n")
+        f.write("#SBATCH --job-name= Plasma_"+inputs["setup"]["Runfolder"]+"_1 \n")
+        f.write("#SBATCH --cpus-per-task="+str(inputs["setup"]["cores"])+" \n") #Use shared memory parallel environemnt 
+        f.write("#$SBATCH --ntasks="+str(inputs["setup"]["repeats"])+" \n")
+        if(inputs["run"]["method"]=="QChem"):
+            f.write("module load test qchem \n")
+            f.write("module load qchem \n")
+            f.write("mkdir $TMPDIR/qchemlocal\n")
+            f.write("tar -xzvf /mnt/scratch/"+getpass.getuser()+"/qchem.tar.gz -C $TMPDIR/qchemlocal\n")
+            f.write('qchemlocal=$TMPDIR/qchemlocal\n')
+            f.write('export QCHEM_HOME="$qchemlocal"\n')
+            f.write('export QC="$qchemlocal"\n')
+            f.write('export QCAUX="$QC/qcaux"\n')
+            f.write('export QCPROG="$QC/exe/qcprog.exe"\n')
+            f.write('export QCPROG_S="$QC/exe/qcprog.exe_s"\n')
+            f.write('export PATH="$PATH:$QC/exe:$QC/bin"\n')
+            f.write("export QCSCRATCH="+EXDIR1+"/rep-$SLURM_ARRAY_TASK_ID \n")
+        f.write("unset GOMP_CPU_AFFINITY KMP_AFFINITY \n")    
+        f.write("cd "+EXDIR1+"/rep-$SLURM_ARRAY_TASK_ID \n")
+        f.write("python ./../code/main.py")
+        f.close()
+        plasma_command = ['sbatch', '--dependency=afterok:' + setup_job_id, file1]
+        subprocess.call(plasma_command)
     else: 
         for i in range(inputs["setup"]["repeats"]):
             os.chdir(EXDIR1+"/rep-"+str(i+1))
@@ -186,6 +251,3 @@ if __name__=="__main__":
 
        
         
-          
-        
-    
